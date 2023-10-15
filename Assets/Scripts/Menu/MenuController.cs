e0bbca2bb1da2eb67a1b5d0e60c8f071da7fecaa
GitarisTWorkSpace <spd2020.work@gmail.com>
2023-10-15 16:29:07 +0500

    add game menu

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Slider loadingBar;
+    [SerializeField] private float waitingTime;
+
     void Start()
     {
-        
+        loadingBar.maxValue = 100f;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        StartCoroutine(LoadingNewScene());
+    }
+
+    private void MoveToGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    private IEnumerator LoadingNewScene()
+    {
+        while (loadingBar.value < 100)
+        {
+            loadingBar.value += Random.Range(0, 15);
+            yield return new WaitForSeconds(waitingTime);
+        }
+        MoveToGameScene();
     }
 }
